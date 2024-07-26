ba39191f0e7d4ad14dfdd20d9de423c2a10ba0a9
Optimusik <37900128+Optimusik@users.noreply.github.com>
2024-07-26 21:57:18 +0300

    Add files via upload

---
+++ scripts/deploy.ts
@@ -0,0 +1,29 @@
+import { ethers, run } from 'hardhat';
+
+async function main() {
+  const initialOwner = '0x...'; // Replace with the actual owner address
+
+  console.log('Deploying PrivateNFT...');
+  const Contract = await ethers.getContractFactory('PrivateNFT');
+  const contract = await Contract.deploy(initialOwner);
+
+  await contract.deployed();
+  console.log('PrivateNFT deployed to:', contract.address);
+
+  const contractAddress = contract.address;
+
+  // Write the deployed address to a file (optional)
+  const fs = require('fs');
+  const path = require('path');
+  const deployedAddressPath = path.join(__dirname, '..', 'utils', 'deployed-address.ts');
+  const fileContent = `const deployedAddress = '${contractAddress}'\n\nexport default deployedAddress\n`;
+  fs.writeFileSync(deployedAddressPath, fileContent, { encoding: 'utf8' });
+  console.log('Address written to deployedAddress.ts');
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
